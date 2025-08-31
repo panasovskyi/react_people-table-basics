426a2390912cc8e3b173098f08c07d169798829c
panasovskyi <ie.panasovskyi@gmail.com>
2025-08-31 16:26:45 +0100

    mother/father link error

---
+++ src/store/PeopleContext.tsx
@@ -0,0 +1,38 @@
+import { createContext, useEffect, useState } from 'react';
+import { Person } from '../types';
+import { getPeople } from '../api';
+
+export const PeopleContext = createContext({
+  people: [] as Person[],
+  isLoading: false,
+  errorMessage: '',
+});
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export const PeopleProvider: React.FC<Props> = ({ children }) => {
+  const [people, setPeople] = useState<Person[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    getPeople()
+      .then(setPeople)
+      .catch(() => setErrorMessage('Something went wrong'))
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  const values = {
+    people,
+    isLoading,
+    errorMessage,
+  };
+
+  return (
+    <PeopleContext.Provider value={values}>{children}</PeopleContext.Provider>
+  );
+};
